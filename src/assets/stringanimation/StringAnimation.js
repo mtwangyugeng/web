c0ade72c7ecfb9f744f2f1642ccea9ca01e3f79d
Simon Dickenson <mtwangyugeng@gmail.com>
2021-03-09 18:04:59 -0500

    added string animation asset

---
+++ src/assets/stringanimation/StringAnimation.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default class StringAnimation extends React.Component{
+    /**
+     * @ props.strings : list of strings
+     * 
+     * @ returns react component that plays 
+     *  slide-show on given list of strings.
+     */
+
+    state = {
+        currstring: 0, // current slide
+        content: "",
+        da: false, // deleting = true, adding = false
+    }
+    textref = React.createRef()
+    
+    render() {
+        return(
+        <div ref = {this.textref}>
+            {this.state.content + "_"}
+        </div>
+        )
+    }
+
+    tick = null
+    componentDidMount() {
+        this.tick = setInterval(this.handletick, 300)
+    }
+    handletick = () => {
+        if(this.state.da){
+            if (this.state.content === ""){
+                this.setState({da: false, currstring: (this.state.currstring + 1) % this.props.strings.length})
+            }else{
+                this.setState({content: this.state.content.slice(0,-1)})
+            }
+        }else{
+            if (this.state.content === this.props.strings[this.state.currstring]){
+                clearInterval(this.tick)
+                setTimeout(() => {
+                    this.tick = setInterval(this.handletick, 300)
+                }, 2000);
+                this.setState({da: true})
+            }else{
+                this.setState({content: this.state.content + this.props.strings[this.state.currstring][this.state.content.length]})
+            }
+        }
+    }
+
+
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import StringAnimation from './assets/stringanimation/StringAnimation'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <StringAnimation strings = {["I am Sexy", "and I know it"]}/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
